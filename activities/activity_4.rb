5c8085edbd6484bc431a27157b391752c2e174b9
Jerome Reyes <rjerome415@gmail.com>
2022-06-15 17:43:58 +0800

    activity 2: a program that takes a hash of first, middle, last name and convert it to fullname

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,24 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+def oneTimeArray(ages)
+
+  repeatedNum=[]
+
+  ages.each_with_index do |current_age,current_index|
+    flag=false
+    ages.each_with_index do|age,index|
+      if current_index!=index
+        if current_age==age
+          flag=true
+        end
+      end
+    end
+    if flag==false
+      repeatedNum << current_age
+    end
+  end
+
+  return repeatedNum
+end
+p oneTimeArray([12,12,13,13,90,89,60,90])
