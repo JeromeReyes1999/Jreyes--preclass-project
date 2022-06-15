f8ebc51ecc0d47664a1cacc419a2a24d7102bf62
Jerome Reyes <rjerome415@gmail.com>
2022-06-15 18:00:50 +0800

    activity 3: a program that takes an array of number and returns the differences between the 2 consecutive numbers in the array

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,16 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+#
+
+def ageDifferences(ages)
+  differences=[]
+  ages.each_with_index do |age,index|
+    if index!=0
+      differences << ages[index]-ages[index-1]
+    end
+  end
+  return differences
+end
+
+p ageDifferences([12,13,56,58,34,56,89,19])
