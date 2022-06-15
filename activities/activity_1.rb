2ac1a982a60307843c7e8717a40f94c53d4603c9
Jerome Reyes <rjerome415@gmail.com>
2022-06-15 16:15:06 +0800

    activity 1: a program that filters the adult among the array and computes for the average of the adults' ages

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,16 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+def adultAverageAge(ages)
+  adults = []
+  sum=0
+  ages.each do |age|
+    if age>=18
+      sum=sum+age
+      adults << age
+    end
+  end
+  return sum/adults.size
+end
+
+p adultAverageAge([12,23,25,89,45,43,23,15,18,17])
